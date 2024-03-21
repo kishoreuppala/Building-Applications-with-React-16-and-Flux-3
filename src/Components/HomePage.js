d9da720c8b7d5b7facd7681d5a0dd4297164202f
Kishore .U <nagakishore64@gmail.com>
2024-03-21 10:07:53 +0530

    Added components directory & Homepage.js

---
+++ src/Components/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function HomePage() {
+    <div className='jumbotron'>
+        <h1>Pluralsight React Course</h1>
+        <p>React, Flux, and React Router for ultra responsive web apps.</p>
+    </div>
+}
